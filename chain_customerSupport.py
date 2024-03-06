2e2d99f1fa5db493c79ed083be419377be04d56e
rizkyyuda28 <160560988+rizkyyuda28@users.noreply.github.com>
2024-03-06 11:16:14 +0000

    project finished

---
+++ chain_customerSupport.py
@@ -0,0 +1,24 @@
+import gradio as gr
+from langchain.chains import LLMChain
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+import os
+
+# Mengatur LLM
+os.environ["OPENAI_API_KEY"] = "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"
+llm = ChatOpenAI(temperature=0.9)
+
+def handle_complaint(komplain: str) -> str:
+    #Buat instance LLM dengan nilai temprature 0,9 (nilai lebih tinggi membuat keluaran lebih acak).
+    
+    
+    # Merancang template untuk merespon komplain
+    prompt = PromptTemplate(input_variables=["komplain"], template="Saya seorang perwakilan layanan pelanggan. Saya menerima keluhan berikut: {komplain}. Respon saya adalah:")
+    
+    # Membuat model bahasa berantai dengan template yang telah dirancang
+    chain = LLMChain(llm=llm, prompt=prompt)
+    return chain.run(komplain)
+
+# Membuat antarmuka Gradio untuk fungsi handle_complaint
+iface = gr.Interface(fn=handle_complaint, inputs="text", outputs="text")
+iface.launch(share=True)
